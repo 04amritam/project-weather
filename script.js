7d96c31dbd5ff2312a64610d871a371a31be86c0
04amritam <amritanandpandey@gmail.com>
2023-06-18 14:12:47 +0530

    sunrise sunset updated

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city,opt
 .then(response=>response.json())
 .then((response)=>{
 	console.log(response)
-	
 	temp.innerHTML = response.temp
 	temp2.innerHTML = response.temp
 	min_temp.innerHTML = response.min_temp
@@ -24,10 +23,12 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city,opt
 	wind_speed2.innerHTML = response.wind_speed	
 	wind_degrees.innerHTML = response.wind_degrees
 	cloud_pct.innerHTML = response.cloud_pct
-	humidity.innerHTML = response.humidity
 	humidity2.innerHTML = response.humidity	
 	sunrise.innerHTML = response.sunrise
 	sunset.innerHTML = response.sunset
+
+	humidity.innerHTML = response.humidity
+	
 	
 })
 .catch(err=>console.error(err));
